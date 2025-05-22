715f80a943b4bf16a2a5d7c0f8ecf292fa0a78fe
dlwlgur02 <dlwlgur02@gmail.com>
2025-05-22 15:11:49 +0900

    langchain_gen.py 소스코드 작성

--- langchain_gen_image.py
+++ langchain_gen_image.py
@@ -1,4 +1,7 @@
 import os
+import uuid
+import requests
+from typing_extensions import TypeDict
 from dotenv import find_dotenv, load_dotenv
 from langchain_openai import ChatOpenAI
 from openai import OpenAI
@@ -23,3 +26,61 @@ llm = ChatOpenAI(
 
 # 3. DALL-E3 모델 생성
 client = OpenAI(api_key =openai_api_key)
+
+# 4. LangGragph State 생성
+class State(TypeDict):
+    prompt: str
+    image_url: str
+
+# 5. LangGraph Node 생성
+
+def download_image(image_url: str, save_path: str) -> None:
+    response = requests.get(image_url)
+    if response.status_code == 200:
+        with open(save_path, 'wb') as f:
+            f.write(response.content)
+        print(f"이미지 저장 완료: {save_path}")
+    else:
+        print(f"이미지 다운로드 실패: {response.status_code}")
+
+## 5-1. Prompt 확장
+def refine_prompt(state: State) -> State:
+    user_input = state["prompt"]
+    response = llm.invoke(f"다음 문장을 이미지 생성용으로 개선하고, \
+                          개선한 문장만 출력해줘: {user_input}")
+    print(f"확장 프롬프트: {response.content}")
+    return {"prompt": response.content}
+
+## 5-2 . Prompt 번역
+def translate_prompt(state: State) -> State:
+    prompt = state["prompt"]
+    response = llm.invoke(f"다음 문장을 영어로 번역해줘: {prompt}")
+    print(f"번역 결과: {response.content}")
+    return {"prompt": response.content}
+
+## 5-3. 이미지 생성
+def generate_image(state: State) -> State:
+    prompt = state["prompt"]
+    response = client.images.generate(
+        model="dall-e-3",
+        prompt=prompt,
+        size="512x512",
+        quality="standard",
+        n=1
+    )
+    image_url = response.data[0].url
+    save_path = f"./downloads/{uuid.uuid4()}.png"
+    download_image(image_url, save_path)
+
+    return {"prompt": state["prompt"], "image_url": image_url}
+
+# 6. LangGraph 생성
+workflow = StateGraph(State)
+workflow.add_node("refine_prompt", refine_prompt)
+workflow.add_node("translate_prompt", translate_prompt)
+workflow.add_node("generate_image", generate_image)
+
+workflow.set_entry_point("refine_prompt") # 시작 노드
+workflow.add_edge("refine_prompt", "translate_prompt") # 노드 연결
+workflow.add_edge("translate_prompt", "generate_image") # 노드 연결
+workflow.set_exit_point("generate_image") # 종료 노드
